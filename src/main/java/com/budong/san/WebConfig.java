cf3cb229b323567052bdec29b72d7b0986dfbfd9
rnjsals0905@gmail.com <rnjsals0905@ajou.ac.kr>
2023-11-14 20:35:19 +0900

    CORS 해결

---
+++ src/main/java/com/budong/san/WebConfig.java
@@ -0,0 +1,15 @@
+package com.budong.san;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // cors를 적용할 spring서버의 url 패턴.
+                .allowedOrigins("**") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
+                .allowedMethods("GET","POST","PUT","DELETE"); // cors를 허용할 method
+    }
+}
