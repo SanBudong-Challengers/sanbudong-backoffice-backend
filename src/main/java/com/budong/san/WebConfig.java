30c9cd68846966dfddc5eacc9565260fdf39db2c
rnjsals0905@gmail.com <rnjsals0905@ajou.ac.kr>
2023-11-14 22:06:38 +0900

    CORS 해결#2

--- src/main/java/com/budong/san/WebConfig.java
+++ src/main/java/com/budong/san/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // cors를 적용할 spring서버의 url 패턴.
-                .allowedOrigins("**") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
+                .allowedOrigins("*") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
                 .allowedMethods("GET","POST","PUT","DELETE"); // cors를 허용할 method
     }
 }
